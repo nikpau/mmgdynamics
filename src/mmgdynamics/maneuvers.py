6b99f65b854da79b2de03266672d08856f9a0571
nikpau <n.paulig@web.de>
2022-04-14 12:40:43 +0200

    free flow test

--- src/mmgdynamics/maneuvers.py
+++ src/mmgdynamics/maneuvers.py
@@ -1,20 +1,23 @@
 
 import math
+from matplotlib.patches import Rectangle
 import numpy as np
 import matplotlib.pyplot as plt
+from calibrated_vessels import kvlcc2
 
-from typing import List
+from typing import Dict, List
 from mmgdynamics import step
 
-def turning_maneuver(ivs: np.ndarray, vessel: dict, 
-                     time: int, dir: str = "starboard", 
-                     maxdeg: int=20, water_depth: float = None) -> np.ndarray:
+
+def turning_maneuver(ivs: np.ndarray, vessel: dict,
+                     time: int, dir: str = "starboard",
+                     maxdeg: int = 20, water_depth: float = None) -> np.ndarray:
     """
     Perform a turning maneuver for a given set 
     of inital values and a time horizon. 
     The trajectory returns absolute x and 
     y coordinates assuming the vessel started at the origin.
-    
+
     To reproduce https://doi.org/10.1007/s00773-014-0293-y the rudder steering
     rate has to be 1.75°/s (1.76 in the paper)
 
@@ -30,7 +33,7 @@ def turning_maneuver(ivs: np.ndarray, vessel: dict,
 
     Returns:
         np.ndarray: Array of absolute x and y coordinates for the trajectory
-        
+
     Note:
         There is no option to input curent speeds at the moment
         apart from hard-coding it into the function. Feel free to add the feature
@@ -43,10 +46,10 @@ def turning_maneuver(ivs: np.ndarray, vessel: dict,
     # If maxdeg is set to 35° you have approx
     # 35°/20s=1.75°/s
     delta_list = np.concatenate(
-        [np.array([0.]), 
-         np.linspace(0.00, maxdeg, 20), 
+        [np.array([0.]),
+         np.linspace(0.00, maxdeg, 20),
          np.full(time-20, maxdeg)]
-        )
+    )
 
     if dir == "starboard":
         delta_list = delta_list * np.pi/180.
@@ -61,15 +64,14 @@ def turning_maneuver(ivs: np.ndarray, vessel: dict,
     # Simulate for `time` seconds
     for s in range(time):
 
-
         # Set r to the next value in our list
         ivs[3] = delta_list[s+1]
 
         # Solve the ODE system for one second at a time
-        sol = step(X=ivs, 
-                   params=vessel, 
-                   sps=1, 
-                   nps_old=ivs[4], 
+        sol = step(X=ivs,
+                   params=vessel,
+                   sps=1,
+                   nps_old=ivs[4],
                    delta_old=delta_list[s],
                    fl_vel=None,
                    water_depth=water_depth)
@@ -80,7 +82,7 @@ def turning_maneuver(ivs: np.ndarray, vessel: dict,
         # Transform to earth-fixed coordinate system
 
         psi += r
-        print(s,round(float(r),4),psi*180/math.pi)
+        print(s, round(float(r), 4), psi*180/math.pi)
         v_x = math.cos(psi) * u - math.sin(psi) * v
         v_y = math.sin(psi) * u + math.cos(psi) * v
 
@@ -115,16 +117,17 @@ def plot_trajecory(t: List[np.ndarray], vessel: dict) -> None:
 
     # This is just for the arrows depicting the flow direction
     # For now this is just added manually
-    x,y=np.meshgrid(np.linspace(-5,5,20),np.linspace(-2,4,20))
-    u, v = 0,1
-    plt.quiver(x,y,u,v, scale=100., width = 0.001, color="grey")
+    x, y = np.meshgrid(np.linspace(-5, 5, 20), np.linspace(-2, 4, 20))
+    u, v = 0, 1
+    plt.quiver(x, y, u, v, scale=100., width=0.001, color="grey")
     plt.xlabel(r"$y_0/L$", fontsize=14)
     plt.ylabel(r"$x_0/L$", fontsize=14)
-    plt.title(r"Seiun Maru ($L=105m,B=18m$)$\pm 35^{\circ}$ turning maneuver | $U_0=5.0m/s (\approx 9.72kn), rpm=90$")
+    plt.title(
+        r"Seiun Maru ($L=105m,B=18m$)$\pm 35^{\circ}$ turning maneuver | $U_0=5.0m/s (\approx 9.72kn), rpm=90$")
     plt.axhline(y=0, color='r', linestyle='-')
     plt.axvline(x=0, color='r', linestyle='-')
     plt.grid(True)
-    #plt.savefig(PLOTDIR+"/"+"turning.pdf")
+    # plt.savefig(PLOTDIR+"/"+"turning.pdf")
     plt.show()
 
 
@@ -185,10 +188,10 @@ def zigzag_maneuver(ivs: np.ndarray, vessel: dict, max_deg: int, rise_time: int)
         ivs[3] = delta_list[s+1]
 
         # Solve the ODE system for one second at a time
-        sol = step(X=ivs, 
-                   params=vessel, 
-                   sps=1, 
-                   nps_old=ivs[4], 
+        sol = step(X=ivs,
+                   params=vessel,
+                   sps=1,
+                   nps_old=ivs[4],
                    delta_old=delta_list[s],
                    fl_vel=None,
                    water_depth=None)
@@ -218,7 +221,7 @@ def plot_zigzag(t: list, delta_list: np.ndarray) -> None:
     plt.ylabel(r"$\psi (blue) \qquad \delta (orange)$", fontsize=14)
     plt.title(r"ZigZag Maneuver with $\pm 20^{\circ}$")
     plt.grid(True)
-    #plt.savefig(PLOTDIR+"/"+"zigzag.pdf")
+    # plt.savefig(PLOTDIR+"/"+"zigzag.pdf")
     plt.show()
 
 
@@ -227,7 +230,120 @@ def plot_r(t: List[float]):
     plt.plot(np.arange(len(t[2])), t[2], linewidth=2.5)
     plt.xlabel(r"$t(s)$", fontsize=14)
     plt.ylabel(r"$r(-)$", fontsize=14)
-    plt.title(r"Yaw rate acceleration $r(-)$ for $\pm 35^{\circ}$ turning maneuver")
+    plt.title(
+        r"Yaw rate acceleration $r(-)$ for $\pm 35^{\circ}$ turning maneuver")
     plt.grid(True)
-    #plt.savefig(PLOTDIR+"/"+"r.pdf")
+    # plt.savefig(PLOTDIR+"/"+"r.pdf")
     plt.show()
+
+
+def free_flow_test(vessel: Dict[str, float]):
+
+    fig = plt.figure()
+    fig.patch.set_facecolor("#212529")
+    ax: plt.Axes = fig.add_subplot(1, 1, 1)
+
+    def deg(a): return a*180/math.pi
+
+    timestep = 0
+    twopi = 2*math.pi
+
+    head = 0.0
+
+    qx, qy = np.linspace(-500, 500, 11), np.linspace(-500, 500, 11)
+
+    agx, agy = 0.0, 0.0
+
+    # Length and Breadth of the simulated vessel
+    L, B = vessel["Lpp"], vessel["B"]
+
+    ivs = np.array(
+        [
+            0.0,  # Longitudinal vessel speed [m/s]
+            0.0,  # Lateral vessel speed [m/s]
+            0.0,  # Yaw rate acceleration [rad/s]
+            0.0,  # Rudder angle [rad]
+            0.0  # Propeller revs [s⁻¹]
+        ]
+    )
+    nps = ivs[-1]
+
+    while True:
+        fl_psi = math.pi
+
+        sol = step(
+            X=ivs,
+            params=vessel,
+            nps_old=nps,
+            delta_old=0.0,
+            fl_psi=ang_diff(head, fl_psi),
+            fl_vel=2.0,
+            water_depth=6.0,
+            sps=1
+        )
+
+        timestep += 1
+        print(round(float(ang_diff(head, fl_psi)*180/math.pi), 3), timestep)
+
+        # Vel in x and y direction (m/s), angular turning rate (rad/s)
+        u, v, r, _, _ = sol
+
+        # Transform to earth-fixed coordinate system
+
+        head =  (head + r) % twopi
+        vy = math.cos(head) * u - math.sin(head) * v
+        vx = math.sin(head) * u + math.cos(head) * v
+
+        agx += vx
+        agy += vy
+
+        anchor = agx - B/2, agy - L/2
+
+        # Set current solution as next initial values
+        ivs = np.hstack(sol)
+
+        # Rectangle of the heading transformed vessel
+        vessel_rect = Rectangle(anchor,
+                                width=vessel["B"],
+                                height=vessel["Lpp"],
+                                rotation_point="center",
+                                angle=((2*math.pi)-head)*180/math.pi,
+                                color="black")
+
+        ax.clear()
+        ax.axhline(y=0, color='r', linestyle='-')
+        ax.axvline(x=0, color='r', linestyle='-')
+        ax.quiver(qx, qy,
+                  np.full((11, 11), -math.sin(fl_psi)),
+                  np.full((11, 11), -math.cos(fl_psi)),
+                  scale=20, headwidth=2)
+        ax.add_patch(vessel_rect)
+        plt.pause(0.01)
+
+
+def abs_ang_diff(a1: float, a2: float) -> float:
+    """Relative angle difference for an angle range of [0,2*pi]
+
+    Args:
+        a1 (float): Angle in radians or degrees
+        a2 (float): Angle in radians or degrees
+
+    Returns:
+        float: absolute diff in angles or degs
+    """
+    def deg(a): return a*180/math.pi
+
+    a1, a2 = deg(a1), deg(a2)
+    a = a1-a2
+    a = (a+180) % 360-180
+
+    return a/180*math.pi
+
+
+def ang_diff(head, str_dir):
+
+    twopi = 2*math.pi
+    return (str_dir - head) % twopi
+
+
+free_flow_test(kvlcc2)
