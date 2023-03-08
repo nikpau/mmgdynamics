0d83e3448e316b1824c1b7f24629277c21f75433
nikpau <n.paulig@web.de>
2023-03-08 09:46:14 +0100

    cleanup

--- src/mmgdynamics/structs.py
+++ src/mmgdynamics/structs.py
@@ -1,8 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional, TypeVar
-
-import numpy as np
-
+from typing import Optional
 
 @dataclass
 class Vessel:
@@ -82,6 +79,7 @@ class Vessel:
     gamma_R: Optional[float] = None # Flow straightening coefficient
     A_R_Ld_em: Optional[float] = None # Fraction of moveable Rudder area to length*draft
     f_alpha: Optional[float] = None # Rudder lift gradient coefficient
+    delta_prop: Optional[float] = None
 
 @dataclass
 class MinimalVessel:
@@ -101,11 +99,11 @@ class MinimalVessel:
     t_P: Optional[float] = None # Thrust deduction factor
 
 
-Surge = TypeVar("Surge",bound=float)
-Sway = TypeVar("Sway",bound=float)
-YawRate = TypeVar("YawRate",bound=float)
-RudderAngle = TypeVar("RudderAngle",bound=float)
-RevPerSecond = TypeVar("RevPerSecond",bound=float)
+Surge        = float
+Sway         = float
+YawRate      = float
+RudderAngle  = float
+RevPerSecond = float
 
 @dataclass
 class InitialValues:
